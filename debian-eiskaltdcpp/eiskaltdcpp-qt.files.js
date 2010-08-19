b39fc76bcd195f67df51d9304e83b8e5f83c515b
Alex_P <almipo@mail.ru>
2010-08-19 20:41:44 +0400

    eiskaltdcpp: Обновление правил

--- debian-eiskaltdcpp/eiskaltdcpp-qt.files.js
+++
@@ -1,8 +0,0 @@
-usr/bin/eiskaltdcpp-qt
-usr/share/applications/eiskaltdcpp-qt.desktop
-usr/share/man/man1/eiskaltdcpp-qt.1.gz
-usr/share/eiskaltdcpp/examples
-usr/share/eiskaltdcpp/icons
-usr/share/eiskaltdcpp/resources
-usr/share/eiskaltdcpp/scripts
-usr/share/eiskaltdcpp/ts
