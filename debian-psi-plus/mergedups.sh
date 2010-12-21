e328cd430f5287f29bfb1203f64789ff019503fb
Alex_P <almipo@mail.ru>
2010-12-21 20:34:58 +0300

    psi-plus: Обновление правил

--- debian-psi-plus/mergedups.sh
+++ debian-psi-plus/mergedups.sh
@@ -4,9 +4,9 @@ export LC_ALL=C
 cd `dirname $0`
 
 diff psi-plus psi-plus-webkit -NPrqs | awk '/identical/ {print $2}' | sed 's|psi-plus/||' | grep -v psi.xpm | grep -v psi.desktop > files.diff
-#создаем структуру каталогов
+#Creating directory structure
 cat files.diff | xargs -I'{}' dirname {} | sort | uniq | xargs -I'{}' mkdir -p psi-plus-common/{}
-#копируем файлы
+#Copying files
 cat files.diff | xargs -I'{}' cp psi-plus/{} psi-plus-common/{}
 cat files.diff | xargs -I'{}' rm psi-plus/{}
 cat files.diff | xargs -I'{}' rm psi-plus-webkit/{}
