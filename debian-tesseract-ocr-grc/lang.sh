3f243a08f6891fafd2deb1b8eda8587a12de2105
Alexander Pozdnyakvo <almipo@mail.ru>
2017-08-29 21:55:05 +0300

    tesseract*: Обновление правил

--- debian-tesseract-ocr-grc/lang.sh
+++ debian-tesseract-ocr-grc/lang.sh
@@ -2,9 +2,9 @@
 
 #grc
 
-LANG_OLD="dan-frak deu-frak equ kur slk-frak tgl"
+LANG_OLD=""
 
-LANG="bul cat ces  dan deu  ell eng fin fra hun ind ita lav lit nld nor pol por ron rus slk slv spa srp swe tur ukr vie chi-sim chi-tra amh asm aze-cyrl bod bos ceb cym dzo fas gle guj hat iku jav kat kat-old kaz khm kir lao lat mar mya nep ori pan pus san sin srp-latn syr tgk tir uig urd uzb uzb-cyrl yid osd afr ara aze bel ben chr enm epo est eus frk frm glg heb hin hrv isl ita-old jpn kan kor mal mkd mlt msa spa-old sqi swa tam tel tha"
+LANG="grc"
 
 CONTROL="control"
 rm -f *.install
