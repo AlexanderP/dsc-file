0d078cab487e5e11fea5656db75c3e2b8b9a8c0e
Alexander Pozdnyakvo <almipo@mail.ru>
2017-09-17 22:00:04 +0300

    tesseract: Обновление правил

--- debian-tesseract-ocr-grc/lang.sh
+++ debian-tesseract-ocr-grc/lang.sh
@@ -29,33 +29,7 @@ Description: tesseract-ocr language files for ${j}
 EOF
 
 cat >> tesseract-ocr-${i}.install << EOF
-best/$(echo ${i} | sed 's/-/_/g').* usr/share/tesseract-ocr/4.00/tessdata/
-
-EOF
-done
-
-for i in ${LANG_OLD} 
-do
-j=$(cat lang.txt | grep "^${i}__" | awk -F '__' '{print $2}')
-cat >> ${CONTROL} << EOF
-Package: tesseract-ocr-${i}
-Architecture: all
-Provides: tesseract-ocr-language, tesseract-ocr-lang
-Depends: \${misc:Depends}
-Recommends: tesseract-ocr (>= 3.04)
-Replaces: tesseract-ocr-data (<< 2)
-Description: tesseract-ocr language files for ${j}
- A commercial quality OCR engine originally developed at HP between 1985
- and 1995. In 1995, this engine was among the top 3 evaluated by UNLV. It
- was open-sourced by HP and UNLV in 2005. This package contains the data
- needed for processing images in a particular language.
-
-EOF
-
-cat >> tesseract-ocr-${i}.install << EOF
 $(echo ${i} | sed 's/-/_/g').* usr/share/tesseract-ocr/4.00/tessdata/
 
 EOF
 done
-
-
