b186a8700012221fca3a85425a7543000586fed1
Alexander Pozdnyakov <almipo@mail.ru>
2018-05-08 18:39:57 +0300

    gimp-gtk3: Обновление правил

--- debian-gimp-gtk3/gimp-console-gtk3.sh
+++ debian-gimp-gtk3/gimp-console-gtk3.sh
@@ -3,7 +3,7 @@
 INSTALL_DIR="/usr/lib/gimpgtk3"
 export LD_LIBRARY_PATH="$INSTALL_DIR/lib:$LD_LIBRARY_PATH"
 export PATH="${PATH}:$INSTALL_DIR/bin"
-export GEGL_PATH="$INSTALL_DIR/lib/gegl-0.3"
+export GEGL_PATH="$INSTALL_DIR/lib/gegl-0.4"
 export BABL_PATH="$INSTALL_DIR/lib/babl-0.1"
 export GIMP3_DATADIR="$INSTALL_DIR/share/gimp/3.0"
 export GIMP3_LOCALEDIR="$INSTALL_DIR/share/locale"
